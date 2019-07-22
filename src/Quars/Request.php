5b3bc47593759cd39c2ae5527a3e0405ecd09479
Miguel Mendoza <miguel.mendoza@pslgroup.com>
2019-07-21 22:55:54 -0500

    Include HttpMethodNotAllowedException to not found error 404.

--- src/Quars/Request.php
+++ src/Quars/Request.php
@@ -10,13 +10,14 @@ namespace Quars;
 
 use Phroute\Phroute\Dispatcher;
 use Phroute\Phroute\Exception\HttpRouteNotFoundException;
+use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
 
 class Request {
 	public static function Serve(){
 		$dispatcher =  new Dispatcher(Route::getRouterObject());
 		try {
 			echo $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-		} catch (HttpRouteNotFoundException $e) {
+		} catch (HttpRouteNotFoundException | HttpMethodNotAllowedException $e) {
 		   	// Error:(404)
 		    header('HTTP/1.0 404 Not Found');
 		    \Quars\Quars::_use('app/Errors/404.php', '',true,'../src/Quars/Messages/Page/404.php');
